97c75e610af2c64c76cc3e6788f9c601b02b0ba2
Raumaan Kidwai <raumaankidwai@gmail.com>
2016-07-12 22:58:08 -0400

    Update jpl.js

--- node/jpl.js
+++ node/jpl.js
@@ -1,10 +1,14 @@
-function jpl (s) {
-	s = s.split(" ");
-	c = s.shift();
-	
-	(a = {
-		"+": function (a) {
-			return a[0] + a[1];
-		}
-	}[c]) ? a(s) : "JPL Error: Function not found";
-}
+var JPL = {
+	exec: function (s) {
+		s = s.split(" ");
+		c = s.shift();
+		
+		(a = {
+			"+": function (a) {
+				return a[0] + a[1];
+			}
+		}[c]) ? a(s) : "JPL Error: Function not found";
+	}
+};
+
+module.exports = JPL;
