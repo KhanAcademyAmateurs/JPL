32fd92511d97c6115c1bd39b9640925e2698cc42
Raumaan Kidwai <raumaankidwai@gmail.com>
2016-08-26 20:57:40 -0400

    Update jpl.js

--- node/jpl.js
+++ node/jpl.js
@@ -3,7 +3,10 @@ module.exports = {
 	err: a => "\033[31mJPL: Error: " + a + "\033[0m",
 	
 	previous: undefined,
-	vars: {},
+	vars: {
+		FALSE: 0,
+		TRUE: 1
+	},
 	cmd: {
 		"p": [[a => a instanceof String ? a : JSON.stringify(a)], (j, a) => a[0]],
 		"+": [[Number, Number], (j, a) => a[0] + a[1]],
@@ -11,15 +14,6 @@ module.exports = {
 		"*": [[Number, Number], (j, a) => a[0] * a[1]],
 		"/": [[Number, Number], (j, a) => a[0] / a[1]],
 		"c": [[String, String], (j, a) => a[0] + a[1]],
-		"g": [[String], function (j, a) {
-			var v = j.vars[a[0]];
-			
-			if (v === undefined) {
-				return j.err("Variable '" + a[0] + "' not found");
-			}
-			
-			return v;
-		}],
 		"s": [[String, a => a], function (j, a) {
 			if (/^[a-z]+$/g.test(a[0])) {
 				return j.err("Variable name '" + a[0] + "' is all lowercase");
@@ -36,9 +30,9 @@ module.exports = {
 			
 			return p;
 		}],
-		"i": [[Boolean, String], function (j, a) {
+		"i": [[a => JSON.parse(String(a).toLowerCase()), String], function (j, a) {
 			if (a[0]) {
-				return j.exec(j, a[1].replace(/,/g, " "))[0];
+				return a[1];
 			} else {
 				return "";
 			}
@@ -56,35 +50,50 @@ module.exports = {
 		var m = j.cmd[c];
 		
 		if (!m) {
-			return [j.err("Function not found: '" + c + "'"), true];
+			return [j.err("Function not found: '" + c + "'"), true, true];
 		}
 		
 		var t = m[0].length;
 		var n = s.length;
 		
 		if (n !== t) {
-			return [j.err("Incorrect number of arguments: '" + c + "' takes " + t + " argument" + j.pl(t)), true];
+			return [j.err("Incorrect number of arguments: '" + c + "' takes " + t + " argument" + j.pl(t)), true, true];
 		}
 		
 		for (var i = 0; i < n; i ++) {
 			var k = s[i];
 			var o = k.split(",");
 			
-			if (o.length < 2 && !/^[a-z]+$/g.test(k) && k[0] !== "\\") {
-				a.push(k);
-			} else if (k[0] === "\\") {
-				a.push(k.slice(1, k.length));
+			if (o.length < 2 && !/^[a-z]+$/g.test(k)) {
+				if (/^[A-Za-z]+$/g.test(k)) {
+					var v = j.vars[k];
+					
+					if (v) {
+						a.push(v);
+					} else {
+						a.push([j.err("Variable '" + k + "' not found"), true, true]);
+					}
+				} else if (k.indexOf("\\") > -1) {
+					a.push(k.replace(/\\/g, ""));
+				} else {
+					a.push(k);
+				}
 			} else {
-				a.push(j.exec(j, o.join(" "))[0]);
+				var l = j.exec(j, o.join(" "));
+				a.push(l[2] ? l : l[0]);
 			}
 		}
 		
 		var r = m[1](j, a.map(function (e, i) {
+			if (typeof e === "object" && e[2]) {
+				return e[0] + ((i + 1) === a.length ? "" : "\n");
+			}
+			
 			return m[0][i](e);
 		}));
 		
 		j.previous = r;
 		
-		return [typeof r === "string" ? r : (r = JSON.stringify(r)), c !== "p" && r.length];
+		return [typeof r === "string" ? r : (r = JSON.stringify(r)), c !== "p" && r.length, false];
 	}
 };
