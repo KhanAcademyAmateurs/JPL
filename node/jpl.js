845be63d4e400dcb69f560e8ff61b0af99b9de47
Raumaan Kidwai <raumaankidwai@gmail.com>
2016-10-16 20:35:21 -0400

    Update jpl.js

--- node/jpl.js
+++ node/jpl.js
@@ -77,7 +77,7 @@ module.exports = {
 			j.vars[a[0]] = a[1];
 			
 			var s = [];
-			
+			console.log(j.exec(j, a[2].replace(/,/g, " "))[0]);
 			while (j.exec(j, a[2].replace(/,/g, " "))[0] === "true") {
 				s.push(j.exec(j, a[4].replace(/,/g, " "))[0]);
 				j.exec(j, "s N " + a[3]);
